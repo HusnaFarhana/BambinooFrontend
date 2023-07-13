cc4d8de0e0fd719379843e2fe3ebaec9e50df74e
HusnaFarhana <Amdazzlin01@gmail.com>
2023-07-14 00:42:03 +0530

    fif

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3500/',
+  // apiUrl: 'http://localhost:3500/',
+  apiUrl: 'https://16.171.173.117/',
   chatUrl: 'http://localhost:4000',
 };
